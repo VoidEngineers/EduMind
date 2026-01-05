1475989dd87036876d6f874e03c8faebdf1fad03
Venath <venathrandima164@gmail.com>
2026-01-05 13:05:00 +0530

    Add LMS frontend

---
+++ apps/web/src/LMS/index.ts
@@ -0,0 +1,6 @@
+export { default } from './LMSHome';
+export { default as LMSHome } from './LMSHome';
+export { default as LMSCourseCard } from './LMSCourseCard';
+export { default as LMSHeader } from './LMSHeader';
+export { default as LMSFooter } from './LMSFooter';
+
